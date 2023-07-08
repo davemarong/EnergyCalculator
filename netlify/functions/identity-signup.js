156797745712b81c43932d7fcb01632797f368e0
DaveTheBrave <kmarong@hotmail.com>
2023-07-08 23:40:44 +0200

    Change role update from app_metadata to root role

--- netlify/functions/identity-signup.js
+++ netlify/functions/identity-signup.js
@@ -1,8 +1,10 @@
 export async function handler(event) {
   const user = JSON.parse(event.body).user;
+  console.log(user);
   return {
     body: JSON.stringify({
       ...user,
+      role: "Premium",
       app_metadata: {
         ...user.app_metadata,
         roles: ["premium"],
